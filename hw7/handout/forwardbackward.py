635873c55eb8dce855ac07da4758f5266967c5c2
vccheng2001 <vccheng@andrew.cmu.edu>
2020-11-13 23:57:05 -0800

    Updated B[;,index]

--- hw7/handout/forwardbackward.py
+++ hw7/handout/forwardbackward.py
@@ -15,7 +15,7 @@ def main():
     # print(A)
     # print(B)
     len_states, len_words = np.size(states), np.size(words)
-    states_dict, words_dict= {}, {}
+    states_dict, word_dict= {}, {}
     # put into states dict, map state to index 
     for i in range(len_states):
         state = states[i]
@@ -23,7 +23,7 @@ def main():
     # put into words dict, map word to index 
     for i in range(len_words):
         word = words[i]
-        words_dict[word] = i
+        word_dict[word] = i
 
     # Estimate yhat_t, assign tags based on state that max P(Yt=sj | x1:T)
     f = open(input_file, "r")
@@ -32,52 +32,62 @@ def main():
         sequence = row.split()
         maxT = len(sequence) 
      
-        alphaMatrix = build_alpha(maxT, states, len_states, prior, B, A)
-        BetaMatrix = build_Beta(maxT, states, len_states, prior, B, A)
+        alphaMatrix = build_alpha(sequence, word_dict, maxT, states, len_states, prior, B, A)
+        BetaMatrix = build_Beta(sequence,word_dict,maxT, states, len_states, prior, B, A)
+        # print(alphaMatrix)
+        # print(BetaMatrix)
+        # exit(0)
         for t in range(maxT):
             alphaBeta = np.multiply(alphaMatrix[t], BetaMatrix[t])
             max_index = np.argmax(alphaBeta)
-            out.write((sequence[t].split('_')[0] + "_" + states[max_index].strip()))
+            out.write((sequence[t].split('_')[0] + "_" + states[max_index].strip() + " "))
         out.write(' \n')
 
 
 # Build alpha
-def build_alpha(maxT,states, len_states, prior, B,A):
+def build_alpha(sequence, word_dict, maxT,states, len_states, prior, B,A):
     alphaMatrix = np.zeros((1,len_states))
     # starting t
     t = 0
-    alphaMatrix = build_alpha_helper(t, maxT, alphaMatrix, states, len_states,  prior, B,A)
+    alphaMatrix = build_alpha_helper(sequence, word_dict, t, maxT, alphaMatrix, states, len_states,  prior, B,A)
     return alphaMatrix
 
 # Build alpha helper function
-def build_alpha_helper(t,maxT, alphaMatrix, states, len_states,  prior, B,A):
+def build_alpha_helper(sequence,word_dict, t, maxT, alphaMatrix, states, len_states,  prior, B,A):
     alphaVec = np.zeros(len_states)
     # Exit case 
    # if (t > t_state): return alphaMatrix
     if (t == maxT): return alphaMatrix
     # Base Case, start from t = 0
+
+    word = sequence[t].split('_')[0]
+    index = word_dict[word]
+
     if (t == 0): # corresponds to t == 1 (0)
         # p(starting state is j) * p(see observation 1 at state j)
-        alphaVec= np.multiply(B[:,t], prior)
+        # print(B[:,t])
+        # print(prior)
+        # print(word, index, sequence[0])
+        alphaVec= np.multiply(B[:,index], prior)
         # Update all states for timestep 1 
         alphaMatrix[0] = alphaVec
-        return build_alpha_helper(t+1, maxT, alphaMatrix, states, len_states,  prior, B,A) 
+        return build_alpha_helper(sequence,word_dict, t+1, maxT, alphaMatrix, states, len_states,  prior, B,A) 
     
-    alphaVec = np.multiply(B[:,t], np.dot(A.T, alphaMatrix[t-1]))
+    alphaVec = np.multiply(B[:,index], np.dot(A, alphaMatrix[t-1]))
     alphaMatrix = np.vstack((alphaMatrix, alphaVec))
-    return build_alpha_helper(t+1, maxT,alphaMatrix, states,len_states, prior, B,A) 
+    return build_alpha_helper(sequence,word_dict, t+1, maxT,alphaMatrix, states,len_states, prior, B,A) 
 
 
-def build_Beta( maxT, states, len_states, prior, B,A):
+def build_Beta(sequence,word_dict,maxT, states, len_states, prior, B,A):
     # init 
     BetaMatrix = np.zeros((1,len_states))
     # start with timestep T
     t = maxT - 1
-    BetaMatrix = build_Beta_helper(t, maxT, BetaMatrix, states, len_states,  prior, B,A)
+    BetaMatrix = build_Beta_helper(sequence,word_dict,t, maxT, BetaMatrix, states, len_states,  prior, B,A)
     return BetaMatrix
 
 # Build Beta
-def build_Beta_helper(t, maxT, BetaMatrix, states, len_states,  prior, B,A):
+def build_Beta_helper(sequence,word_dict,t, maxT, BetaMatrix, states, len_states,  prior, B,A):
     BetaVec = np.zeros(len_states)
     # Exit
     if (t < 0): return BetaMatrix
@@ -85,11 +95,17 @@ def build_Beta_helper(t, maxT, BetaMatrix, states, len_states,  prior, B,A):
     if (t == maxT - 1):
         BetaVec = np.ones(len_states)
         BetaMatrix[0] = BetaVec 
-        return build_Beta_helper(t-1, maxT, BetaMatrix, states, len_states,  prior, B,A) 
+        return build_Beta_helper(sequence,word_dict,t-1, maxT, BetaMatrix, states, len_states,  prior, B,A) 
     # Recurse 
-    BetaVec = np.dot(A, (np.multiply(B[:,t+1], BetaMatrix[0])))
+
+    
+    word = sequence[t].split('_')[0]
+    index = word_dict[word]
+    print("------")
+    print(word, index)
+    BetaVec = np.dot(A, (np.multiply(B[:,index], BetaMatrix[0])))
     BetaMatrix = np.vstack((BetaVec, BetaMatrix)) 
-    return build_Beta_helper(t-1,  maxT, BetaMatrix, states,len_states, prior, B,A) 
+    return build_Beta_helper(sequence,word_dict,t-1,  maxT, BetaMatrix, states,len_states, prior, B,A) 
 
 
    

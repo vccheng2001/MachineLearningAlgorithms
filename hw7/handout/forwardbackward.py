0cddcbe7d8e0199abe6a3a8ef1cb97e352c8b2d8
vccheng2001 <vccheng@andrew.cmu.edu>
2020-11-13 22:00:51 -0800

    debugging alpha'

--- hw7/handout/forwardbackward.py
+++ hw7/handout/forwardbackward.py
@@ -3,14 +3,17 @@ import numpy as np
 
 # Performs forward-backward algorithm 
 def main():
-    (program, validation_input, index_to_word, index_to_tag, hmmprior, hmmemit, hmmtrans, predicted_file, metric_file) = sys.argv
+    (program, input_file, index_to_word, index_to_tag, hmmprior, hmmemit, hmmtrans, predicted_file, metric_file) = sys.argv
     states = np.loadtxt(index_to_tag, dtype='str')
     words = np.loadtxt(index_to_word, dtype='str') 
 
     # Get prior pi, trans A, emit B 
-    prior = np.loadtxt(hmmprior, dtype='float64') 
-    A = np.loadtxt(hmmtrans, dtype='float64')  
-    B = np.loadtxt(hmmemit, dtype='float64')  
+    prior = np.loadtxt(hmmprior, dtype=np.float64) 
+    A = np.loadtxt(hmmtrans, dtype=np.float64) 
+    B = np.loadtxt(hmmemit, dtype=np.float64)  
+    # print(prior)
+    # print(A)
+    # print(B)
 
     len_states, len_words = np.size(states), np.size(words)
     states_dict, words_dict= {}, {}
@@ -24,53 +27,73 @@ def main():
         words_dict[word] = i
 
     # Estimate yhat_t, assign tags based on state that max P(Yt=sj | x1:T)
-    f = open(validation_input, "r")
+    f = open(input_file, "r")
     for row in f:
         sequence = row.split()
-        maxT = len(sequence) 
         # print(sequence)
-        for t_state in range(0,maxT):
+        maxT = len(sequence) 
+        print(sequence)
+        for t_state in range(maxT):
             # Build alpha 
             alphaMatrix = build_alpha(t_state, states, len_states, prior, B, A)
             BetaMatrix = build_Beta(t_state, maxT, states, len_states, prior, B, A)
             alphaBeta = np.multiply(alphaMatrix[t_state - 1], BetaMatrix[t_state - 1])
             max_index = np.argmax(alphaBeta)
-            print(sequence[t_state].split('_')[0] + "_" + states[max_index], end=" ")
-        print('\n')
-    exit(0)
+            #print(sequence[t_state].split('_')[0] + "_" + states[max_index], end=" ")
+        exit(0)
+        # print('\n')
 
 
 # Build alpha
 def build_alpha(t_state, states, len_states, prior, B,A):
+    # print('------------------alpha--------------------')
+    # print('t_state', t_state, '\n')
+    print('prior', prior,'\n')
+    print(prior.shape)
+    print('B', B, '\n')
+    print('A', A,'\n')
     # init 
     alphaMatrix = np.zeros((1,len_states))
     # starting t
     t = 0
     alphaMatrix = build_alpha_helper(t, t_state, alphaMatrix, states, len_states,  prior, B,A)
+    # print("alphaaaa for ", t_state)
+    # print(alphaMatrix)
     return alphaMatrix
 
 # Build alpha helper function
 def build_alpha_helper(t, t_state, alphaMatrix, states, len_states,  prior, B,A):
     alphaVec = np.zeros(len_states)
     # Exit case 
-    if (t == t_state): return alphaMatrix
+    if (t > t_state): return alphaMatrix
 
     # Base Case, start from t = 0
-    if (t == 0):
+    if (t == 0): # corresponds to t == 1 (0)
         # p(starting state is j) * p(see observation 1 at state j)
         alphaVec= np.multiply(B[:,t], prior)
         # Update all states for timestep 1 
         alphaMatrix[0] = alphaVec
         return build_alpha_helper(t+1, t_state, alphaMatrix, states, len_states,  prior, B,A) 
     
-    # Recurse 
-    for j in range(len_states): 
-        aAsum = 0
-        for k in range(len_states):
-            aAsum += np.dot(alphaMatrix[t-1][k], A[k][j])
-        alphaVec[j] = np.multiply(aAsum, B[j][t])
-    # append to old matrix 
+    # Recurse t > 1 to T... 2 to 3 (1 to 2)
+    # for j in range(len_states): 
+    #     aAsum = 0
+    #     for k in range(len_states):
+    #         aAsum += np.dot(alphaMatrix[t-1][k], A[k][j])
+    #     alphaVec[j] = np.multiply(aAsum, B[j][t])
+    # append to old matrix
+   
+    # print(t)
+    # print(B[:,t]) 
+    # print(A)
+    # print(alphaMatrix[t-1])
+    alphaVec = np.multiply(B[:,t], np.dot(A.T, alphaMatrix[t-1]))
+    # print('----')
+    # print(alphaVec)
+    # print(alphaVec)
     alphaMatrix = np.vstack((alphaMatrix, alphaVec))
+    # print(alphaMatrix)
+    #rint(alphaMatrix)
     return build_alpha_helper(t+1, t_state, alphaMatrix, states,len_states, prior, B,A) 
 
 

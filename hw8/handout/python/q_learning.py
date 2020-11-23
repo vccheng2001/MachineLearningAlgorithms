8da6610d46942ec02dfe95b2ce933f47796f936c
vccheng2001 <vccheng@andrew.cmu.edu>
2020-11-22 17:52:10 -0800

    while loop thru episodes

--- hw8/handout/python/q_learning.py
+++ hw8/handout/python/q_learning.py
@@ -6,38 +6,46 @@ import random
 
 
 def main():
-    (program, mode, weight_out, returns_out, episodes, max_iterations, epsilon, gamma, learning_rate) = sys.argv
+    (program, mode, weight_out, returns_out, max_episodes, max_iterations, epsilon, gamma, learning_rate) = sys.argv
     # Initialize mountain car 
     car = MountainCar(mode, 1) # fixes at pos = 0.8, vel = 1
     # Set up
     num_actions, actions = 3, (0,1,2)
     weights = np.zeros((car.state_space, num_actions))
+    num_episodes = 0
     # Do actions
-    while True:
-        prob = random.random()
-        if (prob <= epsilon):
-             action = random.choice(actions)
-        else: 
-            # choose best 
-    # For each action
-    for i in range(num_actions):
-        action = actions[i] 
-        q[s][a] = np.dot(s, weights[:,action]) + b
-        # Observe sample
+    while num_episodes < max_episodes:
+        num_episodes += 1
+        # With prob epsilon, pick random action
+        prob = random.random() 
+        action = random.choice(action) if (prob < epsilon) else getBestAction()
+        # Observe sample 
         (state, reward, done) = car.step(action)
-        pos, vel = state 
-    # 
-    print(state, reward, done)
-    if done:
-        car.reset()
-        car.close()
+        print(state, reward, done)
+        if done:
+            car.reset()
+            car.close()
+            return 
+        else:
+            update_weight()
 
 
-def weightUpdate(learning_rate, gamma, reward):
-    sample = reward + gamma(max...)
-
-    weights = weights - learning_rate()
-
+def update_weight(action, weights, learning_rate, gamma, reward):
+    sample = reward + gamma(maxQnext)
+    currQ = np.dot(s, weights[:,action]) + b
+    weights = weights - learning_rate*(currQ - sample)*gradient
     return weights 
+
+
+def getBestAction():
+    bestAction = None
+    bestQ= float('-inf')
+    for action in allActions:
+        Q = self.getQValue(state,action)
+        if Q >= bestQ:
+        bestAction = action
+        bestQ = Q 
+    return bestAction 
+
 if __name__ == "__main__":
     main()

312f495fc49c1ca0c011de3e7c03dca0391181cc
vccheng2001 <vccheng@andrew.cmu.edu>
2020-11-22 18:06:17 -0800

    num_iters, episodes

--- hw8/handout/python/q_learning.py
+++ hw8/handout/python/q_learning.py
@@ -6,28 +6,30 @@ import random
 
 
 def main():
-    (program, mode, weight_out, returns_out, max_episodes, max_iterations, epsilon, gamma, learning_rate) = sys.argv
+    (program, mode, weight_out, returns_out, episodes, max_iterations, epsilon, gamma, learning_rate) = sys.argv
     # Initialize mountain car 
     car = MountainCar(mode, 1) # fixes at pos = 0.8, vel = 1
     # Set up
     num_actions, actions = 3, (0,1,2)
     weights = np.zeros((car.state_space, num_actions))
-    num_episodes = 0
     # Do actions
-    while num_episodes < max_episodes:
-        num_episodes += 1
-        # With prob epsilon, pick random action
-        prob = random.random() 
-        action = random.choice(action) if (prob < epsilon) else getBestAction()
-        # Observe sample 
-        (state, reward, done) = car.step(action)
-        print(state, reward, done)
-        if done:
-            car.reset()
-            car.close()
-            return 
-        else:
-            update_weight()
+    for episode in range(int(episodes)):
+        num_iters = 0
+        while num_iters < int(max_iterations):
+            # With prob epsilon, pick random action
+            prob = random.random() 
+            action = random.choice(action) if (prob < epsilon) else getBestAction()
+            # Observe sample 
+            (state, reward, done) = car.step(action)
+            print(state, reward, done)
+            if done:
+                car.reset()
+                return 
+            else:
+                update_weight()
+            num_iters += 1
+        car.reset()
+    car.close()
 
 
 def update_weight(action, weights, learning_rate, gamma, reward):

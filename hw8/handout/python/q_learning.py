1b408f7560ab44d8b73c342c160a4df89f7e099f
vccheng2001 <vccheng@andrew.cmu.edu>
2020-11-24 12:54:16 -0800

    how to update state

--- hw8/handout/python/q_learning.py
+++ hw8/handout/python/q_learning.py
@@ -9,13 +9,8 @@ def main():
     epsilon, gamma, alpha, episodes, max_iterations = float(epsilon), float(gamma), float(alpha), int(episodes), int(max_iterations)
     # Set up
     actions, num_actions = (0,1,2), 3 
-    car = MountainCar(mode=mode)
-    state = tuple(car.state)
-    ss = car.state_space
-    Q = {}
     # weights: 2 by 3 matrix 
-    w = np.zeros((ss, num_actions))
-    bias = 0
+    Q, w, bias = {}, np.zeros((2, num_actions)), 0
     # Output files 
     w_out = open(weight_out, 'w')
     r_out = open(returns_out, 'w')
@@ -23,20 +18,22 @@ def main():
     for episode in range(episodes):
         num_iters = 0
         total_rewards = 0
+        car = MountainCar(mode=mode)
+        state = tuple(car.state)
+        ss = car.state_space
         while num_iters < max_iterations:
             num_iters += 1
-            # Init Q[state] for all actions
             Q = init_Q(Q, state, actions)
             # With prob epsilon, pick random action
             prob = random.random() 
             action = random.choice(actions) if (prob < epsilon) else getBestAction(Q, state, actions)
             # Observe sample 
             (next_state, reward, done) = car.step(action)
-            next_state = tuple(next_state)
+            next_state = tuple(next_state.values())
+            Q = init_Q(Q, next_state, actions)
+            #print(state, next_state)
             # Add curr reward 
             total_rewards += reward 
-            # Init next state Q
-            Q = init_Q(Q, next_state, actions)
             # Sample
             sample = reward + gamma * bestQVal(Q, next_state) # get best value
             # Calculate q
@@ -46,16 +43,17 @@ def main():
             diff = Q[state][action] - sample
             w[:,action] = w[:,action] - alpha*diff*w_gradient
             bias =  bias - alpha*diff*1
-            state = next_state
             if done:
                 car.reset()
                 break
+            state = next_state 
         # Print rewards 
         r_out.write(str(total_rewards)+ "\n")
         car.reset()
     # Weight outputs 
     w_out.write(str(bias)+'\n')
     w_list = w.flatten(order='C')
+    print(w)
     for i in w_list:
         w_out.write(str(i) + '\n')
     # Close

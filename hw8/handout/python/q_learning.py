ebfefdf2d8402c56dd9884d194854efb372163fa
vccheng2001 <vccheng@andrew.cmu.edu>
2020-11-24 09:12:45 -0800

    Next state best val

--- hw8/handout/python/q_learning.py
+++ hw8/handout/python/q_learning.py
@@ -5,15 +5,13 @@ import numpy as np
 import random 
 import math 
 
-def map_state_to_index(state):
-    (pos, vel) = state 
-    col = (pos + 1.2)/0.36
-    row = (vel + 0.07)/0.03 
-    print(math.floor(row), math.floor(col))
-def main():
-    map_state_to_index([0.11,-0.05])
+# def map_state_to_index(state):
+#     (pos, vel) = state 
+#     col = (pos + 1.2)/0.36
+#     row = (vel + 0.07)/0.03 
+#     print(math.floor(row), math.floor(col))
 
-def hi():
+def main():
     (program, mode, weight_out, returns_out, episodes, max_iterations, epsilon, gamma, alpha) = sys.argv
     epsilon, episodes, max_iterations = float(epsilon), int(episodes), int(max_iterations)
     # Initialize mountain car 
@@ -25,29 +23,33 @@ def hi():
     bias = 0
     # Do actions
     for episode in range(episodes):
+        state = car.state 
         num_iters = 0
         while num_iters < max_iterations:
             num_iters += 1
             # With prob epsilon, pick random action
             prob = random.random() 
-            action = random.choice(action) if (prob < epsilon) else  0 #getBestAction(Q, state, actions)
+            action = random.choice(actions) if (prob < epsilon) else getBestAction(Q, state, actions)
             # Observe sample 
-            (state, reward, done) = car.step(action)
-            index = map_state_to_index(state)
+            (next_state, reward, done) = car.step(action)
+            sample = reward + gamma * bestQVal(Q, next_state) # get best value
+            w = update_w(Q, state, action, w, bias)
+            Q = update_Q(Q, state, action, w, bias)
+            gradient = state
+            # update weights 
+            w[state][action] = w[state][action] - (alpha * (Q[state][action] - sample)) * gradient
+            bias = bias - (alpha * (Q[state][action] - sample)) * gradient
             if done: car.reset()
-            else:
-                # As array
-                state = np.fromiter(state.values(), dtype=float)
-                # Update q 
-                w = update_w(Q, state, action, w, bias)
-                Q = update_Q(Q, state, action, w, bias)
-                #sample = reward + gamma * (maxQnext)
-                gradient = state
-                # update weights 
-                w[state][action] = w[state][action] - (alpha * (Q[state][action] - sample)) * gradient
-                bias = bias - (alpha * (Q[state][action] - sample)) * gradient
+            else: state = next_state
         car.reset()
     car.close()
+
+def bestQVal(Q, next_state):
+    if not next_state in Q:
+        Q[next_state] = {}
+        return 0
+    else:
+        return max(Q[next_state].values())
 
 def update_w(Q, state, action, w, bias):
     if not state in w:
@@ -60,7 +62,7 @@ def update_w(Q, state, action, w, bias):
 
 def update_Q(Q, state, action, w, bias):
     if not state in Q:
-        Q[state] = []
+        Q[state] = {}
     Q[state][action] = np.dot(state, w[state][action]) + bias
     return Q 
 
@@ -73,16 +75,12 @@ def update_Q(Q, state, action, w, bias):
 #     bias = bias - learning_rate*(currQ - sample)*gradient 
 #     return weights 
 
-# Return Q value given state, action
-def getQValue(Q, state, action):
-    return Q[state][action] 
-
 # Return best action for given state 
-def getBestAction(Q, state, allActions):
+def getBestAction(Q, state, actions):
     bestAction = None
     bestQ= float('-inf')
-    for action in allActions:
-        currQ = getQValue(Q, state,action)
+    for action in actions:
+        currQ = Q[state][action] 
         if currQ >= bestQ:
             bestAction = action
             bestQ = currQ

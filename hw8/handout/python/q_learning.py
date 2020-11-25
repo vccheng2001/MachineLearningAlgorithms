d1e5fa8be5856e7b77eaaa2b8b3b98d1a7087ea5
vccheng2001 <vccheng@andrew.cmu.edu>
2020-11-25 14:11:12 -0800

    Manually tracing through

--- hw8/handout/python/q_learning.py
+++ hw8/handout/python/q_learning.py
@@ -24,6 +24,7 @@ def main():
 
     # Do actions
     for i in range(episodes):
+        print("Episode number %d" % i)
         done = False
         num_iters = 0
         total_rewards = 0
@@ -31,6 +32,7 @@ def main():
         state_dict = car.reset()
         state = tuple(state_dict.values())
         while True:
+            print("\n ITER NUMBER %d \n" % num_iters)
             num_iters += 1
 
             if num_iters > max_iterations:
@@ -40,16 +42,21 @@ def main():
             if not state in Q:
                 # initialize Q values for each action to 0 
                 Q[state] = {0:0, 1:0, 2:0}
+            print("Q is...", Q)
 
             # With prob epsilon, pick random action
             prob = random.random() 
+            print("Prob is...", prob)
             action = random.choice(actions) if (prob < epsilon) else getBestAction(Q, state, actions)
-
+            
             # Observe sample 
             (next_state_dict, reward, done) = car.step(action)
+            print("Nextstate, Reward, Done", next_state_dict, reward, done)
             next_state = tuple(next_state_dict.values())
+            print("Nextstate", next_state)
             # Add curr reward 
             total_rewards += reward 
+            print("Total rewards", total_rewards)
       
             if done: 
                 car.reset() 
@@ -58,23 +65,40 @@ def main():
                 # Sample
                 if not next_state in Q: # Init next_state Q values to 0
                     Q[next_state] = {0:0, 1:0, 2:0}
+                print("Q with ns", Q)
+                print("Gamma", gamma)
                 sample = reward + (gamma * bestQVal(Q, next_state)) # get best value
+                print("bestQNext", bestQVal(Q, next_state))
+                print("Sample", sample)
+
+                print("Taking the dot product of....")
+                print("State as array", np.asarray(state))
+                print("Wa", w[action]) 
+                print("Bias", bias)
                 Q[state][action] = np.dot(np.asarray(state), w[action]) + bias
+                print("Q[state][action]", Q[state][action])
 
                 # Update weights
                 w_gradient = np.asarray(state).T
+                print("Weight gradient", w_gradient)
                 diff = Q[state][action] - sample
+                print("Diff=Q[s][a] - sample", diff)
+                print("Wa", w[action])
+                print("Alpha*diff*w_gradient", alpha*diff*w_gradient)
                 w[action] = w[action] - alpha*diff*w_gradient
+                print("Updated Wa", w[action])
+                print("Updated W", w)
                 bias = bias - alpha*diff*1
+                print("updated bias", bias)
                 state = next_state
-            
         # Print rewards 
         r_out.write(str(total_rewards)+ "\n")
         
     # Print weight outputs 
     w_out.write(str(bias)+'\n')
     for i in w:
-        w_out.write(str(i) + '\n')
+        for j in i:
+            w_out.write(str(j) + '\n')
 
     # Close
     car.close()
@@ -83,10 +107,12 @@ def main():
 
 
 def bestQVal(Q, next_state):
+    print("Q nextstate", Q[next_state])
     return max(Q[next_state].values())
 
 # Return best action for given state 
 def getBestAction(Q, state, actions):
+    print("Getting best action")
     bestAction = None
     bestQ= float('-inf')
     # Loop through actions
@@ -96,6 +122,7 @@ def getBestAction(Q, state, actions):
         if currQ > bestQ:
             bestAction = action
             bestQ = currQ
+    print("Chose action %d" % bestAction)
     return bestAction 
 
 if __name__ == "__main__":

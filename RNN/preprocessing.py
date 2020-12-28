0b4c921c4b4823d2e7d5618c568fdff247eaa654
vccheng2001 <vccheng@andrew.cmu.edu>
2020-12-28 13:54:48 -0800

    RNN LSTM

---
+++ RNN/preprocessing.py
@@ -0,0 +1,46 @@
+
+import pandas as pd 
+import os
+import numpy as np
+
+raw_path = "raw/"
+train_path = "train/"
+# Preprocesses apnea files 
+def main():
+    raw_path = "raw/"
+    for group in ["positive/"]:
+        read_files(raw_path, group)
+    
+def read_files(raw_path, group):
+    # List all files in group 
+    files = os.listdir(raw_path + group)
+    # Read each file 
+    for i in range(len(files)):
+        file_name = files[i] 
+        file_path = raw_path + group + file_name
+        df = pd.read_csv(file_path, sep=",", usecols=[1])
+        df = df.head(3000-1)
+        df.to_csv(train_path + group + "positive_"+str(i)+".txt", index=False)
+     # for file_name in files:
+    #     print(raw_path + group + file_name)
+    #     file_path = raw_path + group + file_name 
+    #     df = pd.read_csv(file_path, sep=",", usecols=[1])
+    #     # First 3000 rows
+    #     df = df.head(3000-1)
+    #     # df = windows(df, 128, 64)
+    #     df.to_csv(train_path + group + file_name, index=False)
+
+# d: dataframe
+# w: window size (# timesteps) = 128
+# t: overlapping factor (50%)  = 64
+def windows(d, w, t):  
+    r = np.arange(len(d))   
+    s = r[::t]   
+    z = list(zip(s, s + w))   
+    f = '{0[0]}:{0[1]}'.format
+    g = lambda t: d.iloc[t[0]:t[1]]   
+    return pd.concat(map(g, z), keys=map(f, z)) 
+
+
+if __name__ == "__main__":
+    main()
---
+++ RNN/rnn.py
@@ -0,0 +1,99 @@
+# lstm model
+import os 
+import numpy as np
+from numpy import mean
+from numpy import std
+from numpy import dstack
+from pandas import read_csv
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import LSTM
+from tensorflow.keras.utils import to_categorical
+from matplotlib import pyplot
+
+output_labels= ["positive", "negative"]
+
+def load_files(label, group, X, y):
+    path = group + '/' +label + '/'
+    files = os.listdir(path)
+    # Load (x, y))
+    for file in files:
+        # Load each x sample 
+        vec = np.loadtxt(path + file,dtype=np.float64)
+        X = np.vstack((X,vec))
+        # Append output class to y vector 
+        label_num = 1 if label == "positive" else 0
+        y = np.hstack((y,label_num))
+    return X, y
+
+
+def load_dataset():
+    # Load Train Data 
+    group = "train"
+    trainy = np.array([],dtype=np.int64)
+    trainX = np.array([], dtype=np.float64).reshape(0,3000)
+    # Load train files 
+    for label in output_labels:
+        trainX, trainy = load_files(label, group, trainX, trainy)
+    trainy = (np.expand_dims(trainy, axis=1))
+    trainX = np.expand_dims(trainX, axis=2)
+   
+    #Load Test data 
+    group = "test"
+    testy = np.array([],dtype=np.int64)
+    testX = np.array([], dtype=np.float64).reshape(0,3000)
+    # Load train files 
+    for label in output_labels:
+        testX, testy = load_files(label, group, testX, testy)
+    testy = (np.expand_dims(testy, axis=1))
+    testX = np.expand_dims(testX, axis=2)
+
+    print("trainy: ", trainy.shape)
+    print("trainX: ", trainX.shape)
+    print("testy: ", testy.shape)
+    print("testX: ", testX.shape)
+
+    return trainX, trainy, testX, testy 
+
+
+# fit and evaluate a model
+def evaluate_model(trainX, trainy, testX, testy):
+    verbose, epochs, batch_size = 0, 15, 64
+    n_timesteps, n_features, n_outputs = trainX.shape[1], trainX.shape[2], trainy.shape[1]
+    
+    model = Sequential()
+    model.add(LSTM(100, input_shape=(n_timesteps,n_features)))
+    model.add(Dropout(0.5))
+    model.add(Dense(100, activation='relu'))
+    model.add(Dense(n_outputs, activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    # fit network
+    model.fit(trainX, trainy, epochs=epochs, batch_size=batch_size, verbose=verbose)
+    # evaluate model
+    _, accuracy = model.evaluate(testX, testy, batch_size=batch_size, verbose=0)
+    return accuracy
+
+# summarize scores
+def summarize_results(scores):
+    print(scores)
+    m, s = mean(scores), std(scores)
+    print('Accuracy: %.3f%% (+/-%.3f)' % (m, s))
+
+# run an experiment
+def run_experiment(repeats=1):
+    # load data
+    trainX, trainy, testX, testy = load_dataset()
+    # repeat experiment
+    scores = list()
+    for r in range(repeats):
+        score = evaluate_model(trainX, trainy, testX, testy)
+        score = score * 100.0
+        print('>#%d: %.3f' % (r+1, score))
+        scores.append(score)
+    # summarize results
+    summarize_results(scores)
+
+# run the experiment
+run_experiment()

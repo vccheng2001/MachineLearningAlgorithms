411705106844a01ee9bcd8d23a374b752457783c
vccheng2001 <vccheng@andrew.cmu.edu>
2021-02-05 18:51:17 -0800

    added rnn_test_only

--- RNN/rnn_test_only.py
+++ RNN/rnn_test_only.py
@@ -1,71 +1,91 @@
-import tensorflow as tf
-from tensorflow import keras
-# lstm model
-import os 
+'''
+rnn_test_only.py
+
+This program makes predictions on files in the 
+test directory using the pre-trained model. 
+** must train first 
+
+params: <apnea_type>, <timesteps>, <threshold>
+Example: python3 rnn_train_and_test.py osa 160 0.9
+'''
+
+import os, sys
 import numpy as np
-import pandas as pd
-from numpy import mean, std, dstack
+from numpy import mean, std, dstack 
 from pandas import read_csv
+
+# Keras LSTM model 
+import tensorflow.keras as keras
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, LSTM
 from tensorflow.keras.utils import to_categorical
 
-timesteps = 120
-threshold = 0.5
+# Graphing 
+from matplotlib import pyplot
+
+# parameters 
+(program, apnea_type, timesteps, threshold) = sys.argv
+test_path = "test_" + apnea_type + '/'
 batch_size = 64
-output_labels= ["positive", "negative"]
+labels = {"positive/":1, "negative/":0}
 
-apnea_type = "osa"
-test_group = "test_" + apnea_type 
-def test():
-    model = keras.models.load_model('trained_'+apnea_type+'_model')
+
+# tests on positive/negative sequences in test files
+def main():
+    # load saved model 
+    model = keras.models.load_model(f"trained_{apnea_type}_model")
+    # load input test vector 
     testX, actual = load_test_dataset()
-    probs = model.predict(testX, batch_size, verbose=1)
-    output_predictions(probs,actual)
+    # predictions based on trained model 
+    predictions = model.predict(testX, batch_size)
+    # save predictions to file 
+    output_predictions(predictions, np.asarray(actual))
    
+# saves predictions 
+def output_predictions(predictions,actual):
+    # number of test files in test dir
+    num_predictions = predictions.shape[0] 
+    # 1 if > threshold, 0 otherwise 
+    flags = np.zeros((num_predictions, 1))
+    for i in range(num_predictions):
+        p = predictions[i]
+        # flag apnea (1) if positive prediction >= threshold, else 0
+        flags[i] = 1 if p[1] >= float(threshold) else 0
+    
+    # Add column for flagged (predicted) values
+    predictions = np.hstack((predictions, flags))
 
-def output_predictions(probs,actual):
-    N = probs.shape[0]
-    pred = np.zeros((N, 1))
-    for i in range(N):
-        row = probs[i]
-        if row[1] >= threshold:
-            pred[i] = 1
-    output = np.hstack((probs, pred))
-
-    # Actual
-    actual = np.asarray(actual)
+    # Add column for actual values
     actual = np.expand_dims(actual, axis=1)
+    predictions = np.hstack((predictions, actual))
 
-    # Add pred, actual columns
-    pred_actual = np.hstack((pred,actual)) 
-    output = np.hstack((probs, pred_actual))
-    np.savetxt('predictions_'+apnea_type+'.txt', output, delimiter=' ',fmt='%10f',header="Negative,Positive,Prediction,Actual")
-    print(output)
+    # save predictions
+    np.savetxt(f'predictions_{apnea_type}.txt', predictions, delimiter=' ',fmt='%10f',header="Negative,Positive,Prediction,Actual")
+    print(predictions)
 
-
-
+# Create test X matrix, actual values
 def load_files_test(actual, label, X):
-    path = test_group + '/' +label + "/"
+    path=test_path+label
     files = os.listdir(path)
     for file in files:
         print('Currently processing test file:', file)
-        vec = np.loadtxt(path + file,delimiter="\n", dtype=np.float64)
-        X = np.vstack((X,vec))
-        if 'positive' in file:
-            actual.append(1)
-        else:
-            actual.append(0)
+        arr = np.loadtxt(path + file,delimiter="\n", dtype=np.float64)
+        # Add as row to x matrix
+        X = np.vstack((X, arr))
+        # Build actual values (positive/negative)
+        actual.append(labels[label])
     return X
 
+# load input test vector as matrix 
 def load_test_dataset():
-    actual= []
-    testX = np.array([], dtype=np.float64).reshape(0,timesteps)
-    # Load train files 
-    for label in output_labels:
+    actual = list()
+    testX = np.array([], dtype=np.float64).reshape(0,int(timesteps))
+    # Load test files 
+    for label in labels:
         testX = load_files_test(actual, label, testX)
     testX = np.expand_dims(testX, axis=2)
-    print("test X ", testX.shape)
-    return testX,actual
+    return testX, actual
 
-test()
+
+if __name__ == "__main__":
+    main()

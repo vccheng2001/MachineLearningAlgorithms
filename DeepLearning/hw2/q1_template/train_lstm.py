bf5eb92a2a1f063f7c59023662c73ca9c2b96a33
vccheng2001 <vccheng@andrew.cmu.edu>
2021-03-22 14:43:40 -0700

    change to 5 epochs

--- DeepLearning/hw2/q1_template/train_lstm.py
+++ DeepLearning/hw2/q1_template/train_lstm.py
@@ -22,7 +22,7 @@ def main():
     test_loader = DataLoader(dataset=test_dataset, batch_size=16, shuffle=False, num_workers=4)
 
     # hyper-parameters
-    num_epochs = 20
+    num_epochs = 5
     lr = 0.005
     input_size = 17 # do not change input size
     hidden_size = 128

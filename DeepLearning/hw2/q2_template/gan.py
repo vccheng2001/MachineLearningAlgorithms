9bf84a9eaacfb00032754c5dcc2b18d45b51b969
vccheng2001 <vccheng@andrew.cmu.edu>
2021-04-03 17:52:00 -0700

    Tune generator params, add # layers

--- DeepLearning/hw2/q2_template/gan.py
+++ DeepLearning/hw2/q2_template/gan.py
@@ -36,24 +36,30 @@ class Generator(nn.Module):
         # your output should be of dim (batch_size, airfoil_dim)
         # you can use tanh() as the activation for the last layer
         # since y coord of airfoils range from -1 to 1
-        # def block(in_feat, out_feat, normalize=True):
-        #     layers = [nn.Linear(in_feat, out_feat)]
-        #     if normalize:
-        #         layers.append(nn.BatchNorm1d(out_feat, 0.8))
-        #     layers.append(nn.LeakyReLU(0.2, inplace=True))
-        #     return layers
+
+        self.latent_dim = latent_dim
+        self.airfoil_dim = airfoil_dim
+
+        def block(in_feat, out_feat, normalize=True):
+            layers = [nn.Linear(in_feat, out_feat)]
+            if normalize:
+                layers.append(nn.BatchNorm1d(out_feat, 0.8))
+            layers.append(nn.LeakyReLU(0.2, inplace=True))
+            return layers
 
         self.model = nn.Sequential(
-            nn.Linear(latent_dim, 64),
-            nn.ReLU(),
-            nn.Linear(64, 200),
+            *block(latent_dim, 32, normalize=False),
+            *block(32, 64),
+            *block(64, 128),
+            nn.Linear(128, self.airfoil_dim),
             nn.Tanh()
         )
     
+    # Input shape: (batch_size x latent_dim) output shape: (batch_size x airfoil_dim)
     def forward(self, x): # input x: batch_size x latent_dim
         # print(f"Input to Generator Shape: {x.shape}") # 16x16
-        # define your feedforward pass
         generated = self.model(x)
+        # print(f"Generated shape: {generated.shape}") # 16 x 200
         # x = x.view(img.size(0), *img_shape)
         return generated
 
